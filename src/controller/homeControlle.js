c5cd473cda3dedb7c55c2393eb1c01258e69d100
Tran Duc Manh <tranducmanh235@gmail.com>
2022-08-20 08:05:01 +0700

    http request method & use Database

--- src/controller/homeControlle.js
+++ src/controller/homeControlle.js
@@ -1,7 +1,25 @@
+import connection from "../config/connectDB";
+
 let getHomepage = (req, res) => {
     // logic
+    let data = [];
+    connection.query("SELECT * FROM `users`", function (err, results, fields) {
+        console.log(">>>check mysql");
+        console.log(results);
+        results.map((row) => {
+            data.push({
+                id: row.id,
+                email: row.email,
+                address: row.address,
+                firstName: row.firstName,
+                lastName: row.lastName,
+            });
+        });
 
-    return res.render("index.ejs");
+        // console.log(">>check data inside: ", data);
+
+        return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+    });
 };
 
 module.exports = {
--- src/route/web.js
+++ src/route/web.js
@@ -4,13 +4,15 @@ import homeController from "../controller/homeControlle";
 let router = express.Router();
 
 const initWebRoute = (app) => {
+    // app.METHOD(PATH, HANDLER);
+
     router.get("/", homeController.getHomepage);
 
     router.get("/about", (req, res) => {
         res.send("I'm Tranducmanhx4!");
     });
 
-    // tien to truoc router
+    // prefix befor router
     return app.use("/", router);
 };
 
